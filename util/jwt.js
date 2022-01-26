4544f833bd8f8c100c0b5f081a6d4371fad0921e
Ashutosh Muley <ashutosh.m@fyle.in>
2022-01-27 00:18:31 +0530

    added schema validation for auth api

--- util/jwt.js
+++ util/jwt.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import SECRETS  from "./config";
+import SECRETS from "./config";
 
 export const newToken = (user) => {
   return jwt.sign({ id: user._id }, SECRETS.jwt, {
