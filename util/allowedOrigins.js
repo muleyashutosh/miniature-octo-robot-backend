0b5d68699123e9a35f5a27f9bc8996089605b4c7
Ashutosh S Muley <58973401+muleyashutosh@users.noreply.github.com>
2022-02-27 20:24:10 +0530

    added allowed origins in cors options (#13)

---
+++ util/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+  'https://app.elcarto.xyz', // future site url 
+  'http://localhost:3001', // @Todo: for development only, remove in prod - @Ashu
+  'http://localhost:3000' // ^ same.
+];
+
+export default allowedOrigins;
