df5b3fde8141a88da022983b27ce4ec6302f14a8
Sakshamyadav19 <66155369+Sakshamyadav19@users.noreply.github.com>
2022-04-02 11:08:13 +0530

    upload docs via ipfs (#20)
    * upload docs via ipfs
    
    * added access token as cookie support
    
    Co-authored-by: Ashutosh Muley <muleyashutosh@gmail.com>

--- resources/transactions/transactions.controller.js
+++ resources/transactions/transactions.controller.js
@@ -1,37 +1,39 @@
 import randToken from 'rand-token'
+import { create } from "ipfs-http-client";
 
+async function ipfsClient() {
+  const ipfs = await create(
+    {
+      host: "ipfs.infura.io",
+      port: 5001,
+      protocol: "https"
+    }
+  );
+  return ipfs;
+}
+
+const sampleData = [];
 
 // dummy api.
 const getAllTransaction = (req, res) => {
-  const sampleData = [
-    {
-      name: "abcd.jpg",
-      timestamp: new Date(),
-      hash: randToken.uid(256)
-    },
-    {
-      name: "abcd2.jpg",
-      timestamp: new Date(),
-      hash: randToken.uid(256)
-    },
-    {
-      name: "saksham.jpg",
-      timestamp: new Date(),
-      hash: randToken.uid(256)
-    },
-    {
-      name: "ashu.pdf",
-      timestamp: new Date(),
-      hash: randToken.uid(256)
-    },
-    {
-      name: "monica.jpg",
-      timestamp: new Date(),
-      hash: randToken.uid(256)
-    },
-  ]
 
   res.json(sampleData)
 }
 
-export { getAllTransaction };
+const addTransaction = async (req, res) => {
+  console.log("########################")
+  console.log(req.headers)
+  res.json("ok")
+  let ipfs = await ipfsClient();
+
+  let data = req.files.doc.data
+  let options = {
+    warpWithDirectory: false,
+    progress: (prog) => console.log(`Saved :${prog}`)
+  }
+  let result = await ipfs.add(data, options);
+
+  console.log(result.path)
+  sampleData.push({ hash: result.path, name: req.files.doc.name, timestamp: new Date() })
+}
+export { getAllTransaction, addTransaction };
--- resources/transactions/transactions.router.js
+++ resources/transactions/transactions.router.js
@@ -1,8 +1,11 @@
 import {Router} from 'express'
-import {getAllTransaction} from './transactions.controller'
+import fileupload from 'express-fileupload'
+import {getAllTransaction, addTransaction} from './transactions.controller'
 
 const router = Router();
+router.use(fileupload());
 
-router.route("/").get(getAllTransaction)
+
+router.route("/").get(getAllTransaction).post(addTransaction)
 
 export default router;
