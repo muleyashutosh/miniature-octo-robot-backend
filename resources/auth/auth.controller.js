6d4d769e11e78414c09378b3993061d9746976cd
Sakshamyadav19 <sakshamyadavpune@gmail.com>
2022-04-13 20:42:31 +0530

    integrated hyperledger fabric with backend

--- resources/auth/auth.controller.js
+++ resources/auth/auth.controller.js
@@ -4,7 +4,22 @@ import { User } from "../user/user.model";
 import { UserApiSigninSchema, UserApiSignupSchema } from "../user/user.schema";
 import { RefreshToken } from './refreshToken.model';
 import { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRY, REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRY } from '../../util/config';
+import { Gateway, Wallets } from 'fabric-network';
+import FabricCAServices from 'fabric-ca-client';
+import path from 'path';
+import { buildCAClient, registerAndEnrollUser, enrollAdmin } from '../../../../test-application/javascript/CAUtil.js';
+import { buildCCPOrg1, buildWallet } from '../../../../test-application/javascript/AppUtil.js';
+import { fileURLToPath } from 'url';
+import { ccp, caClient, wallet } from '../../server'
+let contract, userId;
+export { contract, userId };
 
+const channelName = 'mychannel';
+const chaincodeName = 'basic';
+const mspOrg1 = 'Org1MSP';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const walletPath = path.join(__dirname, 'wallet');
 
 const signup = async (req, res) => {
 
@@ -37,6 +52,22 @@ const signup = async (req, res) => {
       httpOnly: true,
       maxAge: 60 * 60 * 1000
     });
+
+    
+    await registerAndEnrollUser(caClient, wallet, mspOrg1, user[0]._id.toString(), 'org1.department1');
+    const gateway = new Gateway();
+
+    await gateway.connect(ccp, {
+      wallet,
+      identity: user[0]._id.toString(),
+      discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+    });
+    // Build a network instance based on the channel where the smart contract is deployed
+    const network = await gateway.getNetwork(channelName);
+
+    // Get the contract from the network.
+    contract = network.getContract(chaincodeName);
+    userId = user[0]._id.toString();
 
     return res.status(201).send({ status: "ok", accessToken });
   } catch (e) {
@@ -88,6 +119,20 @@ const signin = async (req, res) => {
       httpOnly: true,
       maxAge: 60 * 60 * 1000
     });
+
+    const gateway = new Gateway();
+
+    await gateway.connect(ccp, {
+      wallet,
+      identity: user._id.toString(),
+      discovery: { enabled: true, asLocalhost: true } // using asLocalhost as this gateway is using a fabric network deployed locally
+    });
+    // Build a network instance based on the channel where the smart contract is deployed
+    const network = await gateway.getNetwork(channelName);
+
+    // Get the contract from the network.
+    contract = network.getContract(chaincodeName);
+    userId = user._id.toString();
 
     return res.json({ status: "ok", accessToken });
   } catch (e) {
@@ -189,8 +234,6 @@ const logout = async (req, res) => {
   return res.sendStatus(204);
 
 }
-
-
 
 
 export { signup, signin, refresh, reject, logout };
--- resources/transactions/transactions.controller.js
+++ resources/transactions/transactions.controller.js
@@ -1,5 +1,9 @@
 import randToken from 'rand-token'
 import { create } from "ipfs-http-client";
+import { Gateway, Wallets } from 'fabric-network';
+import { ccp, caClient, wallet } from '../../server'
+import { contract, userId } from '../auth/auth.controller'
+import { json } from 'express';
 
 async function ipfsClient() {
   const ipfs = await create(
@@ -12,16 +16,29 @@ async function ipfsClient() {
   return ipfs;
 }
 
-const sampleData = [];
+function prettyJSONString(inputString) {
+  return JSON.stringify(JSON.parse(inputString), null, 2);
+}
+
+const channelName = 'mychannel';
+const chaincodeName = 'basic';
+
 
 // dummy api.
-const getAllTransaction = (req, res) => {
-
-  res.json(sampleData)
+const getAllTransaction = async (req, res) => {
+  const docs = [];
+  console.log('\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger');
+  let ledger = await contract.evaluateTransaction('GetAllAssets');
+  ledger = JSON.parse(ledger.toString())
+  for (var asset of ledger) {
+    if (asset["Owner"] == userId) {
+      docs.push({ hash: asset["ID"], name: asset["Name"], timestamp: asset["TimeStamp"] })
+    }
+  }
+  res.json(docs)
 }
 
 const addTransaction = async (req, res) => {
-  console.log("########################")
   console.log(req.headers)
   res.json("ok")
   let ipfs = await ipfsClient();
@@ -33,7 +50,35 @@ const addTransaction = async (req, res) => {
   }
   let result = await ipfs.add(data, options);
 
-  console.log(result.path)
-  sampleData.push({ hash: result.path, name: req.files.doc.name, timestamp: new Date() })
+  console.log('\n--> Submit Transaction: CreateAsset, creates new asset with ID, color, owner, size, and appraisedValue arguments');
+  let asset = await contract.submitTransaction('CreateAsset', result.path, req.files.doc.name, new Date(), userId);
+  console.log('*** Result: committed');
+  if (`${asset}` !== '') {
+    console.log(`*** Result: ${prettyJSONString(asset.toString())}`);
+  }
+  // result = await contract.evaluateTransaction('ReadAsset', 'QmSxcM6u9ff45gb1cm7eZLX5A2NEHwfw');
+  // console.log(typeof (JSON.parse(result)["Sharedwith"]));
+  // return res.json("Asset Created")
 }
-export { getAllTransaction, addTransaction };
+
+const deleteTransaction = async (req, res) => {
+  console.log(req.params);
+  const result = await contract.submitTransaction('DeleteAsset', req.params.id);
+  console.log(`*** Result: ${prettyJSONString(result.toString())}`);
+  return res.json(`Deleted ${req.params.id}`)
+}
+
+const shareDocument = async (req, res) => {
+  try {
+    console.log('\n--> Submit Transaction: UpdateAsset asset70, asset70 does not exist and should return an error');
+    await contract.submitTransaction('UpdateAsset', req.body.hash, req.body.sharedWith);
+    var result = await contract.evaluateTransaction('ReadAsset', 'QmSxcM6u9ff45gb1cm7eZLX5A2NEHwfw');
+    console.log(JSON.parse(result));
+    console.log('******** FAILED to return an error');
+  } catch (error) {
+    console.log(`*** Successfully caught the error: \n    ${error}`);
+  }
+}
+
+
+export { getAllTransaction, addTransaction, deleteTransaction, shareDocument };
